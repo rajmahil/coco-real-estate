07f8eb02cfe19b56b48ec44cee60234871f338ad
Raj <webtest10111011@gmail.com>
2022-05-31 11:44:50 -0600

    update

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -70,9 +70,10 @@ const ProjectDetails = ({ project }) => {
       <PageWrap>
         <LongDescription>{project?.projectLongDescription}</LongDescription>
         <PhotoWrap>
-          {project?.projectImages?.map((image) => (
-            <ProjectImage src={image?.url} key={image?.id} />
-          ))}
+          {projects &&
+            project?.projectImages?.map((image) => (
+              <ProjectImage src={image?.url} key={image?.id} />
+            ))}
         </PhotoWrap>
         <ViewAllProjects />
       </PageWrap>
