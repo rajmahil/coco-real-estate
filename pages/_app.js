509f3c255782c9c70b4115a43b4c53c20464d5c2
Raj <webtest10111011@gmail.com>
2022-06-01 18:52:18 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,34 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/dwl6tjb.css" />
       </Head>
