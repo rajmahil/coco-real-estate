612de41a800eda8572a4dca777c76dc03801a7a6
Raj <webtest10111011@gmail.com>
2022-07-22 14:56:23 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ["res.cloudinary.com", "uploads-ssl.webflow.com"],
+    domains: ["res.cloudinary.com"],
   },
 };
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// module.exports = withBundleAnalyzer({});
