87b2d4a20fd666fb7b397f3872c0f0439abd1549
Raj <webtest10111011@gmail.com>
2022-07-29 16:02:04 -0600

    update

--- firebase/firebaseClient.js
+++ firebase/firebaseClient.js
@@ -4,12 +4,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAyLRsDXxCbwBNewOglTykWVrV4_Qb8lMw",
+  authDomain: "coco-realestate.firebaseapp.com",
+  projectId: "coco-realestate",
+  storageBucket: "coco-realestate.appspot.com",
+  messagingSenderId: "813915140614",
+  appId: "1:813915140614:web:0b016577e90309ca7e4987",
+  measurementId: "G-CTC29H6XLD",
 };
 
 if (!firebase.apps.length) {
